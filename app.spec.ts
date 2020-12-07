ab87839b7a50eca76d3012001e82c2240a6b54c3
Mark Baydoun <markosbo95@gmail.com>
2020-12-07 20:00:58 +0100

    finished I think

--- app.spec.ts
+++ app.spec.ts
@@ -1,2 +1,37 @@
 import {useCheckout} from "./app";
 
+describe("useCheckout function", () => {
+
+  const {scan, remove, total, basket} = useCheckout()
+
+  scan("TSHIRT");
+  scan("TSHIRT");
+  scan("TSHIRT");
+  scan("VOUCHER");
+  scan("VOUCHER");
+  scan("MUG");
+
+  it("Should add scanned items to the basket basket", () => {
+    expect(basket).toHaveProperty("VOUCHER", 2)
+    expect(basket).toHaveProperty("MUG", 1)
+    expect(basket).toHaveProperty("TSHIRT", 3)
+  })
+
+  it("Should return the total value of the items in the basket, before and after the discounts apply", () => {
+    expect(total().preDiscount).toBe(77.5)
+    expect(total().postDiscount).toBe(69.5)
+  })
+
+  it("Should be able to remove items one by one", () => {
+    remove("MUG");
+    expect(basket).not.toHaveProperty("MUG", 1)
+
+    remove("VOUCHER");
+    expect(basket).toHaveProperty("VOUCHER", 1)
+  })
+
+  it("Should get the correct totals after the removal", () => {
+    expect(total().preDiscount).toBe(65)
+    expect(total().postDiscount).toBe(62)
+  })
+})
--- app.ts
+++ app.ts
@@ -1,76 +1,77 @@
-/*
-  Products:
-  - Voucher: 5 €
-  - Tshirt: 20 €
-  - Mug: 7.5 €
-
-  VOUCHER items have a 2 for 1 offer.
-  TSHIRT items have a 1 € reduction on unit price when buying 3 or more.
-
-*/
-
 import products from "./data/products.json";
 import promotions from "./data/promotions.json";
 import discountTypes from "./data/discountTypes.json"
 
-type ProductKey = keyof typeof products; // the SKUs for each product from the json file.
-type PromotionType = keyof typeof promotions; // Object for each promotion that we add to the store, with the type of discount and object it applies to.
-type DiscountTypeKey = keyof typeof discountTypes; // type of discount, where it explains how each works and describes the params. If made more complex,
-                                                   // it could be used to create discounts automatically (like 3 for 1, etc) according to more parameters, but I wanted to keep it simple for now.
+export type ProductKey = keyof typeof products; // the SKUs for each product from the json file.
+export type PromotionType = keyof typeof promotions; // Object for each promotion that we add to the store, with the type of discount and object it applies to.
+export type DiscountTypeKey = keyof typeof discountTypes; // type of discount, where it explains how each works and describes the params. If made more complex,
+                                                          // it could be used to create discounts automatically (like 3 for 1, etc) according to more parameters, but I wanted to keep it simple for now.
 
 const applyAllPromotionsAutomatically = (basket: Partial<{[key in ProductKey]: number;}>, total: number) => {
   for (const prod in basket) { 
     const currProdPrice = products[<ProductKey>prod].price
+
     if (basket[<ProductKey>prod] !== undefined) {
+
       for (const promo in promotions) { 
         const currPromo = promotions[<PromotionType>promo]
 
         if (prod === currPromo.productKey) { // for each product we search for promotions that apply to it
+
           switch (currPromo.discount.type) {
+
             case "bogof":
-              // @ts-ignore Same error as in line 34.
+              // @ts-ignore Same error as in line 55.
               total -= Math.floor(basket[<ProductKey>prod] / 2) * currProdPrice
-              console.log("TOTAL", total)
-              return total
+
             case "bulk":
-              // @ts-ignore Same error as in line 34.
-              if (currPromo.discount.minAmount && basket[<ProductKey>prod] >= currPromo.discount.minAmount) {
-                // @ts-ignore Same error as in line 34.
+              // @ts-ignore Same error as in line 55.
+              if (basket[<ProductKey>prod] >= currPromo.discount.minAmount) {
+                // @ts-ignore Same error as in line 55.
                 total -= basket[<ProductKey>prod] * currPromo.discount.discountPerUnit
               }
-              return total
           }
+
         }
       }
     }
   }
+  return total;
 }
 
 const useCheckout = () => {
   const basket: Partial<{[key in ProductKey]: number;}> = {};
 
   const scan = (prodName: ProductKey) => {
-    // @ts-ignore Some weird error. Typescript gives me the error for basket[prodName] where
-    // "Object is possibly undefined" when it clearly isn't, and the functionality still works.
+    // @ts-ignore Same error as in line 55.
     if (basket[prodName] !== undefined) basket[prodName] += 1;
     else {
       basket[prodName] = 1;
     }
   }
 
+  const remove = (prodName: ProductKey) => {
+    // @ts-ignore Some weird error. Typescript gives me the error for basket[prodName] where
+    // "Object is possibly undefined" when it clearly isn't, and the functionality still works, gotta look into it.
+    if (basket[prodName] === 1) delete basket[prodName];
+    // @ts-ignore Same error as in line 55.
+    else if (basket[prodName] !== undefined) basket[prodName] -= 1;
+    else return "No items of that type are in the basket"
+  }
+
   const total = () => {
     let preTotal = 0;
     for (const prod in basket) {
-      // @ts-ignore Same error as in line 34.
+      // @ts-ignore Same error as in line 55.
       preTotal += products[<ProductKey>prod].price * basket[<ProductKey>prod]
     }
 
     const total = applyAllPromotionsAutomatically(basket, preTotal)
 
-    return total
+    return {preDiscount: preTotal, postDiscount: total}
   }
 
-  return {scan, total};
+  return {scan, remove, basket, total};
 }
 
 export {useCheckout}
